cabd831747a7b43742c697fa4db5f71c4a5935ae
Ewen <ewen.herve.35550@gmail.com>
2026-07-23 19:00:10 +0200

    refactor: cookie banner

--- components/common/cookies-consent.tsx
+++ components/common/cookies-consent.tsx
@@ -54,20 +54,21 @@ export default function CookieConsent({ gaId }: { gaId: string }) {
       {consent === null && (
         <div className="fixed z-50 bottom-0 right-0 left-0 bg-[#e2e8f6] font-[epilogue] py-4 px-4 md:px-20 flex flex-col md:flex-row items-center justify-between gap-4">
           <p className="flex-1">
-            J’utilise des cookies pour mieux comprendre l’audience de mon site.
+            J’utilise des cookies pour mieux comprendre l’audience de mon site
+            :)
           </p>
           <div className="flex gap-8">
             <button
-              className="text-white text-xl font-black bg-red px-3 py-2 rounded-sm -rotate-2 cursor-pointer"
+              className="text-white text-xl font-black bg-red px-3 py-2 rounded-sm -rotate-2 hover:rotate-0 transition-transform duration-300 cursor-pointer"
               onClick={() => handleChoice("accepted")}
             >
               Avec plaisir !
             </button>
             <button
-              className="text-[#808397] text-xl font-medium cursor-pointer"
+              className="text-[#808397] hover:text-black-blue font-medium transition-colors duration-300 cursor-pointer"
               onClick={() => handleChoice("refused")}
             >
-              Refuser
+              Non merci
             </button>
           </div>
         </div>
